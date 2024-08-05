bc6b8ad0bcb25116fc24e270a075b6ab75e2df0b
furkan eriç <furkaneric@gmail.com>
2024-08-05 18:50:55 +0300

    first commit

---
+++ BlogMvcApp/Models/Blog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogMvcApp.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Baslık { get; set; }
+        public string Acıklama { get; set; }
+        public string Icerik { get; set; }
+        public DateTime EklemeTarihi { get; set; }
+        public bool Onay { get; set; }
+        public bool Anasayfa { get; set; }
+
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
---
+++ BlogMvcApp/Models/BlogInitializer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BlogMvcApp.Models
+{
+    public class BlogInitializer : DropCreateDatabaseIfModelChanges<BlogContext>
+
+    {
+        protected override void Seed(BlogContext context)
+        {
+            List<Category> Kategory = new List<Category>()
+            {
+
+                new Category() { KategoriAdi = "C#" },
+                new Category() { KategoriAdi = "Asp.nt Mvc" },
+                new Category() { KategoriAdi = "Asp.net web form" },
+                new Category() { KategoriAdi = "Windows form" },
+            };
+
+            foreach (var item in Kategory) { 
+            
+            context.Kategoriler.Add(item);
+            
+            }
+            context.SaveChanges();
+
+
+            List<Blog> bloglar = new List<Blog>()
+            {
+                new Blog() { Baslık = "C# Delegets Hakkında", Acıklama = "C# Delegets Hakkında", EklemeTarihi = DateTime.Now.AddDays(-10), Anasayfa = true, Onay = true, Icerik = "C# Delegets HakkındaC# Delegets Hakkında", CategoryId = 1 },
+                new Blog() { Baslık = "C# Delegets Hakkında", Acıklama = "C# Delegets Hakkında", EklemeTarihi = DateTime.Now.AddDays(-10), Anasayfa = false, Onay = true, Icerik = "C# Delegets HakkındaC# Delegets Hakkında", CategoryId = 2 },
+                new Blog() { Baslık = "C# Delegets Hakkında", Acıklama = "C# Delegets Hakkında", EklemeTarihi = DateTime.Now.AddDays(-10), Anasayfa = true, Onay = true, Icerik = "C# Delegets HakkındaC# Delegets Hakkında", CategoryId = 1 },
+                new Blog() { Baslık = "C# Delegets Hakkında", Acıklama = "C# Delegets Hakkında", EklemeTarihi = DateTime.Now.AddDays(-10), Anasayfa = true, Onay = true, Icerik = "C# Delegets HakkındaC# Delegets Hakkında", CategoryId = 2 },
+                new Blog() { Baslık = "C# Delegets Hakkında", Acıklama = "C# Delegets Hakkında", EklemeTarihi = DateTime.Now.AddDays(-10), Anasayfa = false, Onay = true, Icerik = "C# Delegets HakkındaC# Delegets Hakkında", CategoryId = 1 }, };
+                
+
+            foreach (var item in bloglar)
+            {
+                context.Bloglar.Add(item);
+            }
+            context.SaveChanges();
+
+            base.Seed(context);
+        }
+
+    }
+}
